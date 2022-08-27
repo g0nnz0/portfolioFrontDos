c92d9bfc181915598d48068add5a4b9c3ea1c2b4
g0nnz0 <bonzai_rock@hotmail.com>
2022-08-27 02:25:48 -0300

    Logré que el titulo debajo de mi nombre y la descripcion se traigan desde la db, agregué iconos de bootstrap por npm, luego los usar'e para editar y borrar

--- src/app/model/persona.model.ts
+++ src/app/model/persona.model.ts
@@ -3,10 +3,14 @@ export class persona{
     nombre: string;
     apellido: string;
     img: string;
+    descripcion: string;
+    titulo: string;
 
-    constructor(nombre: string, apellido: string, img: string){
+    constructor(nombre: string, apellido: string, img: string, descripcion: string, titulo: string){
         this.nombre = nombre;
         this.apellido = apellido;
         this.img = img; 
+        this.descripcion = descripcion;
+        this.titulo = titulo;
     }
 }
