ba32c7ed6edf6b3f84b674497b49a631493c3cd5
g0nnz0 <bonzai_rock@hotmail.com>
2022-08-30 16:56:30 -0300

    MVC de class proyectos creado exitosamente, crud en postman OK, en el front me traé los archivos correctamente, aun faltan los componentes para editar y sumar nuevos proyectos, este commit es de emergencia, se me tilda la pc

--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Proyectos } from 'src/app/model/proyectos';
+import { AuthServiceService } from 'src/app/service/auth-service.service';
+import { ProyectosService } from 'src/app/service/proyectos.service';
 
 @Component({
   selector: 'app-proyectos',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./proyectos.component.css']
 })
 export class ProyectosComponent implements OnInit {
+  isLogged = false;
+  pro: Proyectos[] = [];
 
-  constructor() { }
+  constructor(private proServ: ProyectosService, 
+    private auth: AuthServiceService) {this.auth.stateUser().subscribe( res =>{
+      if(res){
+        this.isLogged = true;
+        console.log('Logueado, Botones "Cerrar Sesión", "Nuevo Proyecto", "Editar Proyecto", "Eliminar Proyecto" ACTIVOS');
+      }else{
+        this.isLogged = false;
+        console.log('No Logueado, Botones "Cerrar Sesión", "Nuevo Proyecto", "Editar Proyecto", "Eliminar Proyecto" INACTIVOS');
+      }
+    }) }
 
   ngOnInit(): void {
+    this.cargarProyecto()
   }
 
+  cargarProyecto(): void{
+    this.proServ.lista().subscribe(data =>{this.pro = data});
+  }
+
+  borrarProyecto(id?: number): void{
+    if(id != undefined){
+      this.proServ.delete(id).subscribe(data =>{
+        this.cargarProyecto();
+        alert('Proyecto Eliminado.')
+        console.log('Proyecto Eliminado.')
+      }, err =>{
+        alert('No se pudo eliminar el Proyecto Seleccionado.')
+      })
+    }
+  }
 }
---
+++ src/app/service/proyectos.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Proyectos } from '../model/proyectos';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProyectosService {
+
+  proURL = 'http://localhost:8080/proyectos'
+
+  constructor(private httpClient: HttpClient) { }
+
+  public lista(): Observable<Proyectos[]>{
+    return this.httpClient.get<Proyectos[]>(this.proURL + '/lista');
+  }
+
+  public detail(id: number): Observable<Proyectos>{
+    return this.httpClient.get<Proyectos>(this.proURL + `/detail/${id}`);
+  }
+  
+  public save(pro: Proyectos): Observable<any>{
+    return this.httpClient.post<any>(this.proURL + '/crear', pro);
+  }
+
+  public update(id: number, pro: Proyectos): Observable<any>{
+    return this.httpClient.put<any>(this.proURL + `/actualizar/${id}`, pro);
+  }
+
+  public delete(id: number): Observable<any>{
+    return this.httpClient.delete<any>(this.proURL + `/borrar/${id}`);
+  }
+}
